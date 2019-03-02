8e93b82a05378e6d50795663ef6d44401ae8e29f
song <songgalmegi@naver.com>
2019-03-02 17:55:30 +0900

    react에서 store subscribe하기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux";
 
 class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.props.username.user}</p>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+// props에 store를 맵핑
+// [props name] : state.[store name]
+const mapStateToProps = (state) => ({
+  products: state.productReducer,
+  username: state.userReducer
+});
+
+// connect 함수를 통해  props에다가 store를 연결
+export default connect(mapStateToProps)(App);
+// export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./redux/store"; // store.js를 찾고 없으면 store/index.js파일을 찾는다.
 
-
 // HOC방식으로 app을 감싸서 store를 바인딩
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
