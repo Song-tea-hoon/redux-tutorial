f2f5159fa1ac1ead63617ca0fb43c9a11bfc82c3
song <songgalmegi@naver.com>
2019-03-02 16:07:44 +0900

    store생성 및 redux등록, state조회

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+
+const reducer = () => {
+  return 'State'
+}
+const store = createStore(reducer); // redux를 store안에 넣어줘야한다..
+
+console.log(store, store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
