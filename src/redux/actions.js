49ddb119aa1d1a249756ad91e9803ad9ed5c40e0
song <songgalmegi@naver.com>
2019-03-02 18:02:37 +0900

    react에서 action dispatch해보기

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
-export const updateUserAction = {
+// 단순 Action객체에서 Action creater(function)로 변환
+// 단순 객체를 리턴할때보다 확장성이 더욱 올라간다.
+export const updateUser = (name) => ({
   type: 'updateUser',
-  payload: {
-    user: 'Tom'
-  }
-};
+  payload: {user: name}
+});
